85e87fde2cd03f55fb92785944a97aaca3bf4121
Nischaymrzn <nischaymaharjann@gmail.com>
2025-02-06 15:25:09 +0545

    fix: cloudinary upload

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -1,5 +1,6 @@
 import cloudinary from "cloudinary"
 import dotenv from "dotenv"
+import fs from "fs";
 
 dotenv.config()
 
@@ -12,6 +13,7 @@ cloudinary.config({
 export const uploadToCloudinary = (filePath) => {
     return new Promise((resolve, reject) => {
       cloudinary.v2.uploader.upload(filePath, (error, result) => {
+        fs.unlinkSync(filePath);
         if (error) {
             console.log(error)
             reject(error)}
