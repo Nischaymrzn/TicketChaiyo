32c706653209719a12e1eddb8bae66ad00d9b144
Nischaymrzn <nischaymaharjann@gmail.com>
2025-02-16 23:19:03 +0545

    feat: Event Checkout Page

---
+++ client/src/pages/client/EventCheckoutPage.tsx
@@ -0,0 +1,11 @@
+import ClientNav from "./_components/ClientNav"
+
+export const EventCheckoutPage = () => {
+  return (
+    <div className='flex flex-col w-full min-h-[100vh] bg-[#13131A] overflow-hidden'>
+       <ClientNav />
+
+       <p className="p-4 px-10 text-white">Event Checkout Page</p>
+    </div>
+  )
+}
